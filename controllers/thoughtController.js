88497f49b8c3bcbcef0fd01d926b885b77a0a95d
Abdirahman Shukri <abdirahmanshukri@Abdirahmans-MacBook-Air.local>
2023-05-25 12:55:55 -0500

    thoughts

---
+++ controllers/thoughtController.js
@@ -0,0 +1,70 @@
+const {Thought , User} =require('../models');
+const thoughtController = {
+    // get all thoughts
+    getAllThoughts(req,res){
+        Thought.find()
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+            });
+            },
+            // get single thought by id
+            getThoughtById({params},res){
+                Thought.findOne({_id:params.id})
+                .then(dbThoughtData => res.json(dbThoughtData))
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+                    });
+                    },
+                    // create thought
+                    createThought({body},res){
+                        Thought.create(req.body)
+                        .then(({_id}) => {
+                            return User.findOneAndUpdate(
+                                {_id:body.userId},
+                                {$push:{thoughts:_id}}
+                                )
+                                })
+                                .then(dbThoughtData => res.json(dbThoughtData))
+                                .catch(err => res.status(400).json(err));
+                                },
+                                // update thought
+                                updateThought({params,body},res){
+                                    Thought.findOneAndUpdate({_id:params.id},body,{new:true,runValidators:true})
+                                    .then(dbThoughtData => res.json(dbThoughtData))
+                                    .catch(err => res.status(400).json(err));
+                                    },
+                                    // delete thought
+                                    deleteThought({params},res){
+                                        Thought.findOneAndDelete({_id:params.id})
+                                        .then(dbThoughtData => res.json(dbThoughtData))
+                                        .catch(err => res.status(400).json(err));
+                                        },
+                                        // add reaction
+                                        addReaction({params,body},res){
+                                            Thought.findOneAndUpdate(
+                                                {_id:params.thoughtId},
+                                                {$push:{reactions:body}},
+                                                {new:true,runValidators:true}
+                                                )
+                                                .then(dbThoughtData => res.json(dbThoughtData))
+                                                .catch(err => res.status(400).json(err));
+                                                },
+                                                // delete reaction
+                                                deleteReaction({params},res){
+                                                    Thought.findOneAndUpdate(
+                                                        {_id:params.thoughtId},
+                                                        {$pull:{reactions:{reactionId:params.reactionId}}},
+                                                        {new:true}
+                                                        )
+                                                    .then(dbThoughtData => res.json(dbThoughtData))
+                                                    .catch(err => res.status(400).json(err));
+                                                    },
+                                                    };
+                                            
+                                            
+
+                                                
+        
